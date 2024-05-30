1efc150e397093041649776057a9b62ad5164e80
root <root@ip-172-31-4-118.ap-south-1.compute.internal>
2024-05-30 09:37:03 +0000

    Your commit message here

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,8 @@ FRONTEND_URL = os.environ["FRONTEND_URL"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = [ '13.200.229.214']
+
 
 # Application definition
 
